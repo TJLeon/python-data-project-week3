32c42310aa56310725183eb7dcab3125e33b5c80
LeonTan <leotan@student.42kl.edu.my>
2026-05-29 06:16:02 +0800

    feat: working frontend hello world with uvicorn and docker

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -4,6 +4,7 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 templates = Jinja2Templates(directory="src/templates")
 
+
 @app.get("/")
 async def read_root(request: Request):
-	return templates.TemplateResponse(name="chat_page.html", request=request)
+    return templates.TemplateResponse(name="chat_page.html", request=request)
