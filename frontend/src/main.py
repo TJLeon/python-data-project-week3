c5e142bc4b619c0e4ffff0155f001f4eb8285819
LeonTan <leotan@student.42kl.edu.my>
2026-05-28 18:29:07 +0800

    init

---
+++ frontend/src/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="src/templates")
+
+@app.get("/")
+async def read_root(request: Request):
+	return templates.TemplateResponse(name="chat_page.html", request=request)
